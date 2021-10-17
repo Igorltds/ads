0a2487f6d5c644b3ef36ee704304f986684a14d9
Igor Luan Teles de Souza <igorltds@outlook.com>
2021-10-17 01:48:06 -0300

    removendo para renomear e oroganizar

--- MD/AC2/ac2-e02.kt
+++
@@ -1,6 +0,0 @@
-class Aluno (id:Int, nome: String, curso:String) {
-    init{print ("teste $id $nome $curso")}
-}
-
-
-fun main () {Aluno(1, "igor", "ads")}
--- MD/AC2/ac2-e08.kt
+++
@@ -1,4 +0,0 @@
-fun main () {
-    val lambda01 = {a: Int, b:Int -> println(a/b)}
-    lambda01(4, 4)
-}
